a961c5899e72d7209e40aa3f481f1ccd29f7b728
ShekharKG <shekhar114@gmail.com>
2015-10-15 08:58:53 +0530

    CollageView added with touch listener

--- app/src/main/java/com/shekharkg/collageview/MainActivity.java
+++ app/src/main/java/com/shekharkg/collageview/MainActivity.java
@@ -2,6 +2,12 @@ package com.shekharkg.collageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.shekharkg.collageview.toucher.MultiTouchListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ImageView imageView1 = (ImageView) findViewById(R.id.collageView1);
+    ImageView imageView2 = (ImageView) findViewById(R.id.collageView2);
+    ImageView imageView3 = (ImageView) findViewById(R.id.collageView3);
+    ImageView imageView4 = (ImageView) findViewById(R.id.collageView4);
+
+    imageView2.setOnTouchListener(new MultiTouchListener());
+    imageView1.setOnTouchListener(new MultiTouchListener());
+    imageView3.setOnTouchListener(new MultiTouchListener());
+    imageView4.setOnTouchListener(new MultiTouchListener());
+    
   }
+
 }
